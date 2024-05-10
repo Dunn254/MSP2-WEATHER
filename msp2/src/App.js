4e443722c853f33c5e984055bb264a1ac9b6d3c4
Aaren Vo <aarenjvo@gmail.com>
2024-05-09 21:21:40 -0700

    created fetch request for weather, search bar can get data in console and render

--- msp2/src/App.js
+++ msp2/src/App.js
@@ -1,17 +1,24 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 import Splash from './components/splash';
 import LoginForm from './components/login';
 import SignupForm from './components/signup';
 import AppDashboard from './components/dashboard';
+import GetWeather from './components/WeatherApp/WeatherDisplay';
 
 
 function App() {
   return (
-    //<Splash />
+    <div>
+    {/* //<Splash />
     //<LoginForm />
-    //<SignupForm />
-    <AppDashboard />
+    //<SignupForm /> */}
+      <AppDashboard />
+      <div>
+        <GetWeather />
+      </div>
+    </div>
   );
 }
 
