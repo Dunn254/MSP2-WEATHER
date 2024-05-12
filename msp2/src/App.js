a1acc3453ba0093417fa900328ae611d052591df
“Anthony <anthony.deguire619@gmail.com>
2024-05-11 22:45:33 -0700

    added news api

--- msp2/src/App.js
+++ msp2/src/App.js
@@ -1,18 +1,27 @@
-import './App.css';
-import Splash from './components/splash';
-import LoginForm from './components/login';
-import SignupForm from './components/signup';
-import AppDashboard from './components/dashboard';
-import React from 'react'
+import { useState, useEffect } from "react";
 
+const URL = "https://newsapi.org/v2/everything?q=football&apiKey=c451b8a2d2b94b59a64ecba595a58187"
 
 function App() {
+
+  const [article, setArticle] = useState(0)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(URL)
+      console.log(result.json());
+    }
+    fetchData();
+  }, [])
+
   return (
-    //<Splash />
-    //<LoginForm />
-    //<SignupForm />
-    <AppDashboard />
+    <div className="App">
+      Today's News: {article}
+    </div>
   );
 }
 
 export default App;
+
+
+
