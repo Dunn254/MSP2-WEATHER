8e0cecb55f57bc36b843a66c6f5a562066564b72
Dunn254 <124111950+Dunn254@users.noreply.github.com>
2024-05-14 19:30:15 -0700

    frontend and backend separated and running yay!

--- msp2/src/components/WeatherApp/WeatherDisplay.jsx
+++ msp2/src/components/WeatherApp/WeatherDisplay.jsx
@@ -1,100 +1,62 @@
-
-import React, { useState, useEffect } from 'react'
+import React,{ useState, useEffect } from 'react'
 import './WeatherApp.css'
-
-import './WeatherApp.css'
-import React, { useState, useEffect } from 'react'
 import Sun from './Assets/sunny_icon.png'
 import { FaSearch } from 'react-icons/fa'
 
+function WeatherDisplay() {
 
-function GetWeather() {
-    // Create state to store weather data
-    const [weatherData, setWeatherData] = useState(null)
-    const [location, setLocation] = useState('')
-    const [error, setError] = useState(null)
-    // Create a state to track whether data is being fetched or not
-    const [isFetching, setIsFetching] = useState(false)
+     // Create state to store weather data
+     const [weatherData, setWeatherData] = useState(null)
+     const [location, setLocation] = useState('')
+     const [error, setError] = useState(null)
+     // Create a state to track whether data is being fetched or not
+     const [isFetching, setIsFetching] = useState(false)
+ 
+     useEffect(() => {
+ 
+         async function fetchWeatherData() {
+ 
+             // API request options
+             const options = { method: 'GET', headers: { accept: 'application/json' } }
+ 
+             // create fetch url with API key
+             const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
+             const url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`
+             // Fetch weather data
+             try {
+                 const response = await fetch(url, options)
+                 const data = await response.json()
+                 setWeatherData(data)
+                 console.log(data)
+             } catch (err) {
+                 setError(err)
+                 console.log(err)
+             } finally {
+                 setIsFetching(false)
+             }
+         }
+ 
+         // Fetch weather data only if location is not empty
+         if (location.trim() !== '' && isFetching) {
+             fetchWeatherData()
+         } else {
+             // setWeatherData(null)
+             return
+         }
+     }, [location, isFetching])
+ 
+     const handleFetchWeather = () => {
+         setIsFetching(true)
+         setWeatherData(weatherData)
+         console.log(weatherData)
+         console.log('Successfully grabbed weather data for a city ✅')
+     }
+ 
+  return (
+    <div>
 
-    useEffect(() => {
 
-        async function fetchWeatherData() {
-
-            // API request options
-            const options = { method: 'GET', headers: { accept: 'application/json' } }
-
-            // create fetch url with API key
-            const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
-            const url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`
-            // Fetch weather data
-            try {
-                const response = await fetch(url, options)
-                const data = await response.json()
-                setWeatherData(data)
-                console.log(data)
-            } catch (err) {
-                setError(err)
-                console.log(err)
-            } finally {
-                setIsFetching(false)
-            }
-        }
-
-        // Fetch weather data only if location is not empty
-        if (location.trim() !== '' && isFetching) {
-            fetchWeatherData()
-        } else {
-            // setWeatherData(null)
-            return
-        }
-    }, [location, isFetching])
-
-    const handleFetchWeather = () => {
-        setIsFetching(true)
-        setWeatherData(weatherData)
-        console.log(weatherData)
-        console.log('Successfully grabbed weather data for a city ✅')
-    }
-
-    return (
-
-            <div className='container'>
-                <h2>Weather Data:</h2>
-                <form onSubmit={(e) => e.preventDefault()}>
-                    <input
-                        value={location}
-                        type='text'
-                        placeholder='Enter a location'
-                        onChange={(event) => setLocation(event.target.value)} />
-                    <button type='submit' onClick={handleFetchWeather} disabled={!location.trim()}>
-                        {isFetching ? 'Fetching...' : 'Get weather'}
-                    </button>
-                </form>
-                {/* <div className='weatherData'>
-                {weatherData && (
-                        <pre>{JSON.stringify(weatherData)}</pre>
-                )}
-                {error && <p>Error fetching data {error.message}</p>}
-                </div> */}
-                <div className='weatherData'>
-                {weatherData && (
-                        <div>
-                            <p>Location: {weatherData.location.name}</p>
-                            <p>Temperature: {weatherData.current.temp_c}°C</p>
-                            <p>Humidity: {weatherData.current.humidity}</p>
-                            <p>Condition: {weatherData.current.condition.text}</p>
-                            </div>
-                )}
-                {error && <p>Error fetching data {error.message}</p>}
-                </div>
-            </div>
-    )
-
-}
-
-export default GetWeather;
-
-        <div>
+<div>
             <h2>Weather Data:</h2>
             <div className='searchBarContainer'>
                 <form onSubmit={(e) => e.preventDefault()}>
@@ -127,9 +89,65 @@ export default GetWeather;
                 {error && <p>Error fetching data {error.message}</p>}
             </div>
         </div>
-    )
 
+    </div>
+  )
 }
 
-export default GetWeather
+export default WeatherDisplay
+
+
+
+
+
+// import './WeatherApp.css'
+// import React, { useState, useEffect } from 'react'
+
+
+
+// function GetWeather() {
+   
+//     return (
+
+//             <div className='container'>
+//                 <h2>Weather Data:</h2>
+//                 <form onSubmit={(e) => e.preventDefault()}>
+//                     <input
+//                         value={location}
+//                         type='text'
+//                         placeholder='Enter a location'
+//                         onChange={(event) => setLocation(event.target.value)} />
+//                     <button type='submit' onClick={handleFetchWeather} disabled={!location.trim()}>
+//                         {isFetching ? 'Fetching...' : 'Get weather'}
+//                     </button>
+//                 </form>
+//                 {/* <div className='weatherData'>
+//                 {weatherData && (
+//                         <pre>{JSON.stringify(weatherData)}</pre>
+//                 )}
+//                 {error && <p>Error fetching data {error.message}</p>}
+//                 </div> */}
+//                 <div className='weatherData'>
+//                 {weatherData && (
+//                         <div>
+//                             <p>Location: {weatherData.location.name}</p>
+//                             <p>Temperature: {weatherData.current.temp_c}°C</p>
+//                             <p>Humidity: {weatherData.current.humidity}</p>
+//                             <p>Condition: {weatherData.current.condition.text}</p>
+//                             </div>
+//                 )}
+//                 {error && <p>Error fetching data {error.message}</p>}
+//                 </div>
+//             </div>
+//     )
+
+// }
+
+// export default GetWeather;
+
+//     )
+
+// }
+
+// export default GetWeather
 
