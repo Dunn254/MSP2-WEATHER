2b94b820a2a265d221ad958cc7c14746e5820d6e
aarenjvo <aarenjvo@gmail.com>
2024-05-19 09:47:51 -0700

    removed excessive grid items in widgets

--- msp2/src/components/WeatherApp/Widgets.jsx
+++ msp2/src/components/WeatherApp/Widgets.jsx
@@ -27,9 +27,6 @@ const Widgets = ({ humidity, wind_mph, uv, precip }) => {
                     <h3><TbUvIndex /></h3>
                     <h3>UV Index</h3>
                     <h2>{uv}</h2>
-                    {/* <h3><FaWind /></h3>
-                    <h3>wind</h3>
-                    <h3>{wind_mph}mph</h3> */}
                 </div>
             </div>
             <div className='subWidgets'>
@@ -37,26 +34,6 @@ const Widgets = ({ humidity, wind_mph, uv, precip }) => {
                     <h3><TiWeatherShower /></h3>
                     <h3>Precipitation</h3>
                     <h4>{precip} in</h4>
-                </div>
-            </div>
-            <div className='subWidgets'>
-                <div className='subWidgetIcons'>
-                    <h3><WiHumidity /></h3>
-                    <h3>humidity</h3>
-                    <h3>{humidity}%</h3>
-                    {/* <h3><FaWind /></h3>
-                    <h3>wind</h3>
-                    <h3>{wind_mph}mph</h3> */}
-                </div>
-            </div>
-            <div className='subWidgets'>
-                <div className='subWidgetIcons'>
-                    <h3><WiHumidity /></h3>
-                    <h3>humidity</h3>
-                    <h3>{humidity}%</h3>
-                    {/* <h3><FaWind /></h3>
-                    <h3>wind</h3>
-                    <h3>{wind_mph}mph</h3> */}
                 </div>
             </div>
         </div>
