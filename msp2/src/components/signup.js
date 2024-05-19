55bd92feb7c62d61d9a70031ca72099d055c781c
aarenjvo <aarenjvo@gmail.com>
2024-05-19 10:43:40 -0700

    fixed port for backend

--- msp2/src/components/signup.js
+++ msp2/src/components/signup.js
@@ -1,22 +1,12 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 const SignupForm = () => {
-    const navigate = useNavigate();
-
-    const initialFormData = {
+    const [formData, setFormData] = useState({
         username: '',
         email: '',
         password: '',
-        confirmPassword: '',
-        zipCode: '',
-        state: '',
-        country: '',
-        favoriteSport: '',
-        favoriteStock: ''
-    };
-
-    const [formData, setFormData] = useState(initialFormData);
+        confirmPassword: ''
+    });
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -28,32 +18,28 @@ const SignupForm = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        
         try {
-            console.log('Form Data:', formData); // Check the formData being sent
-            
-            const response = await fetch('http://localhost:5000/signup', {
+            const response = await fetch('http://localhost:5001/signup', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(formData)
             });
-    
-            if (response.ok) {
-                alert('Signup successful!');
-                setFormData(initialFormData);
-                navigate('/login'); // Redirect to login page after successful signup
-            } else {
-                setFormData(initialFormData);
-                alert('The account its already registered', response.status, response.statusText);
-            }
+            const data = await response.json();
+            alert(data.message); // Display success message
+            // Clear form fields after successful signup
+            setFormData({
+                username: '',
+                email: '',
+                password: '',
+                confirmPassword: ''
+            });
         } catch (error) {
-            setFormData(initialFormData);
-            alert('Error submitting form:', error);
+            console.error('Signup error:', error);
+            alert('Signup failed. Please try again.');
         }
     };
-    
 
     // Array of U.S. states
     const usStates = [
@@ -86,31 +72,11 @@ const SignupForm = () => {
         "Norway", "Oman", "Pakistan", "Palau", "Palestine State", "Panama","Qatar", "United States of America"
 
     ];
-const sports = ["Football","Basketball", "Baseball", "Hockey", "Tennis", "Soccer", "Formula 1", "UFC", "Boxing", "MMA"];
 
-const stocks = [
-    "MMM", "AOS", "ABT", "ABBV", "ABMD", "ACN", "ATVI", "ADBE", "AAP", "AMD",
-    "AES", "AFL", "A", "APD", "AKAM", "ALK", "ALB", "ARE", "ALXN", "ALGN",
-    "ALLE", "AGN", "ADS", "LNT", "ALL", "GOOGL", "GOOG", "MO", "AMZN", "AMCR",
-    "AEE", "AAL", "AEP", "AXP", "AIG", "AMT", "AWK", "AMP", "ABC", "AME",
-    "AMGN", "APH", "ADI", "ANSS", "ANTM", "AON", "AOS", "APA", "AIV", "AAPL",
-    "AMAT", "APTV", "ADM", "ARNC", "ANET", "AJG", "AIZ", "T", "ATO", "ADSK",
-    "ADP", "AZO", "AVB", "AVY", "BHGE", "BLL", "BAC", "BK", "BAX", "BBT",
-    "BDX", "BRK.B", "BBY", "BIIB", "BLK", "HRB", "BA", "BKNG", "BWA", "BXP",
-    "BSX", "BMY", "AVGO", "BR", "BF.B", "CHRW", "COG", "CDNS", "CPB", "COF",
-    "CPRI", "CAH", "KMX", "CCL", "CAT", "CBOE", "CBRE", "CDW", "CE", "CNC",
-    "CNP", "CTL", "CERN", "CF", "SCHW", "CHTR", "CVX", "CMG", "CB", "CHD",
-    "CI", "XEC", "CINF", "CTAS", "CSCO", "C", "CFG", "CTXS", "CLX", "CME",
-    "CMS", "KO", "CTSH", "CL", "CMCSA", "CMA", "CAG", "CXO", "COP", "ED",
-    "STZ", "COO", "CPRT", "GLW", "CTVA", "COST", "COTY", "CCI", "CSX", "CMI",
-    "CVS", "DHI", "DHR", "DRI", "DVA", "DE", "DAL", "XRAY", "DVN", "FANG",
-    "DLR", "DFS", "DISCA", "DISCK", "DISH", "DG", "DLTR", "D", "DOV", "DOW",
-    "DTE", "DUK", "DRE", "DD", "DXC", "ETFC", "EMN", "ETN", "EBAY", "ECL",
-    "EIX", "EW", "EA", "EMR", "ETR"];
 
     return (
         <div className="container">
-            <h1>Sign Up</h1>
+            <h2>Sign Up</h2>
             <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="username">Username</label>
@@ -210,17 +176,34 @@ const stocks = [
             </select>
         </div>
                 <div className="form-group">
+                    <label htmlFor="favoriteSport">Favorite Sport</label>
+                    <select
+                        className="form-control"
+                        id="favoriteSport"
+                        name="favoriteSport"
+                        value={formData.favoriteSport}
+                        onChange={handleChange}
+                        
+                    >
+                        <option value="">Select Favorite Sport</option>
+                        {/* Add sport options dynamically */}
+                        {/* Example: <option value="football">Football</option> */}
+                    </select>
+                </div>
+                <div className="form-group">
                     <label htmlFor="favoriteStock">Favorite Stock</label>
-                    <input
-                        type="text"
+                    <select
                         className="form-control"
                         id="favoriteStock"
                         name="favoriteStock"
                         value={formData.favoriteStock}
                         onChange={handleChange}
-                        required
+                        
                     >
-                    </input>
+                        <option value="">Select Favorite Stock</option>
+                        {/* Add stock options dynamically */}
+                        {/* Example: <option value="AAPL">Apple Inc.</option> */}
+                    </select>
                 </div>
                 <button type="submit" className="btn btn-primary">Sign Up</button>
             </form>
@@ -230,3 +213,241 @@ const stocks = [
 
 
 export default SignupForm;
+
+
+
+
+
+
+
+// import { useNavigate } from 'react-router-dom';
+
+// const SignupForm = () => {
+//     const navigate = useNavigate();
+
+//     const initialFormData = {
+//         username: '',
+//         email: '',
+//         password: '',
+//         confirmPassword: '',
+//         zipCode: '',
+//         state: '',
+//         country: '',
+//         favoriteSport: '',
+//         favoriteStock: ''
+//     };
+
+//     const [formData, setFormData] = useState(initialFormData);
+
+//     const handleChange = (e) => {
+//         const { name, value } = e.target;
+//         setFormData({
+//             ...formData,
+//             [name]: value
+//         });
+//     };
+
+//     const handleSubmit = async (e) => {
+//         e.preventDefault();
+        
+//         try {
+//             console.log('Form Data:', formData); // Check the formData being sent
+            
+//             const response = await fetch('http://localhost:5000/signup', {
+//                 method: 'POST',
+//                 headers: {
+//                     'Content-Type': 'application/json'
+//                 },
+//                 body: JSON.stringify(formData)
+//             });
+    
+//             if (response.ok) {
+//                 alert('Signup successful!');
+//                 setFormData(initialFormData);
+//                 navigate('/login'); // Redirect to login page after successful signup
+//             } else {
+//                 setFormData(initialFormData);
+//                 alert('The account its already registered', response.status, response.statusText);
+//             }
+//         } catch (error) {
+//             setFormData(initialFormData);
+//             alert('Error submitting form:', error);
+//         }
+//     };
+    
+
+//     // Array of U.S. states
+//     const usStates = [
+//         "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware",
+//         "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky",
+//         "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri",
+//         "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York",
+//         "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island",
+//         "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington",
+//         "West Virginia", "Wisconsin", "Wyoming"
+//     ];
+
+//     const countries = [
+//         "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia",
+//         "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium",
+//         "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei",
+//         "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia", "Cameroon", "Canada", "Central African Republic",
+//         "Chad", "Chile", "China", "Colombia", "Comoros", "Congo (Congo-Brazzaville)", "Costa Rica", "Croatia",
+//         "Cuba", "Cyprus", "Czechia (Czech Republic)", "Democratic Republic of the Congo", "Denmark", "Djibouti",
+//         "Dominica", "Dominican Republic", "East Timor (Timor-Leste)", "Ecuador", "Egypt", "El Salvador",
+//         "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini (fmr. Swaziland)", "Ethiopia", "Fiji", "Finland",
+//         "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea",
+//         "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran",
+//         "Iraq", "Ireland", "Israel", "Italy", "Ivory Coast", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya",
+//         "Kiribati", "Korea, North", "Korea, South", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon",
+//         "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia",
+//         "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia",
+//         "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar (Burma)", "Namibia", "Nauru",
+//         "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Macedonia (fmr. Macedonia)",
+//         "Norway", "Oman", "Pakistan", "Palau", "Palestine State", "Panama","Qatar", "United States of America"
+
+//     ];
+// const sports = ["Football","Basketball", "Baseball", "Hockey", "Tennis", "Soccer", "Formula 1", "UFC", "Boxing", "MMA"];
+
+// const stocks = [
+//     "MMM", "AOS", "ABT", "ABBV", "ABMD", "ACN", "ATVI", "ADBE", "AAP", "AMD",
+//     "AES", "AFL", "A", "APD", "AKAM", "ALK", "ALB", "ARE", "ALXN", "ALGN",
+//     "ALLE", "AGN", "ADS", "LNT", "ALL", "GOOGL", "GOOG", "MO", "AMZN", "AMCR",
+//     "AEE", "AAL", "AEP", "AXP", "AIG", "AMT", "AWK", "AMP", "ABC", "AME",
+//     "AMGN", "APH", "ADI", "ANSS", "ANTM", "AON", "AOS", "APA", "AIV", "AAPL",
+//     "AMAT", "APTV", "ADM", "ARNC", "ANET", "AJG", "AIZ", "T", "ATO", "ADSK",
+//     "ADP", "AZO", "AVB", "AVY", "BHGE", "BLL", "BAC", "BK", "BAX", "BBT",
+//     "BDX", "BRK.B", "BBY", "BIIB", "BLK", "HRB", "BA", "BKNG", "BWA", "BXP",
+//     "BSX", "BMY", "AVGO", "BR", "BF.B", "CHRW", "COG", "CDNS", "CPB", "COF",
+//     "CPRI", "CAH", "KMX", "CCL", "CAT", "CBOE", "CBRE", "CDW", "CE", "CNC",
+//     "CNP", "CTL", "CERN", "CF", "SCHW", "CHTR", "CVX", "CMG", "CB", "CHD",
+//     "CI", "XEC", "CINF", "CTAS", "CSCO", "C", "CFG", "CTXS", "CLX", "CME",
+//     "CMS", "KO", "CTSH", "CL", "CMCSA", "CMA", "CAG", "CXO", "COP", "ED",
+//     "STZ", "COO", "CPRT", "GLW", "CTVA", "COST", "COTY", "CCI", "CSX", "CMI",
+//     "CVS", "DHI", "DHR", "DRI", "DVA", "DE", "DAL", "XRAY", "DVN", "FANG",
+//     "DLR", "DFS", "DISCA", "DISCK", "DISH", "DG", "DLTR", "D", "DOV", "DOW",
+//     "DTE", "DUK", "DRE", "DD", "DXC", "ETFC", "EMN", "ETN", "EBAY", "ECL",
+//     "EIX", "EW", "EA", "EMR", "ETR"];
+
+//     return (
+//         <div className="container">
+//             <h1>Sign Up</h1>
+//             <form onSubmit={handleSubmit}>
+//                 <div className="form-group">
+//                     <label htmlFor="username">Username</label>
+//                     <input
+//                         type="text"
+//                         className="form-control"
+//                         id="username"
+//                         name="username"
+//                         value={formData.username}
+//                         onChange={handleChange}
+//                         required
+//                     />
+//                 </div>
+//                 <div className="form-group">
+//                     <label htmlFor="email">Email</label>
+//                     <input
+//                         type="email"
+//                         className="form-control"
+//                         id="email"
+//                         name="email"
+//                         value={formData.email}
+//                         onChange={handleChange}
+//                         required
+//                     />
+//                 </div>
+//                 <div className="form-group">
+//                     <label htmlFor="password">Password</label>
+//                     <input
+//                         type="password"
+//                         className="form-control"
+//                         id="password"
+//                         name="password"
+//                         value={formData.password}
+//                         onChange={handleChange}
+//                         required
+//                     />
+//                 </div>
+//                 <div className="form-group">
+//                     <label htmlFor="confirmPassword">Confirm Password</label>
+//                     <input
+//                         type="password"
+//                         className="form-control"
+//                         id="confirmPassword"
+//                         name="confirmPassword"
+//                         value={formData.confirmPassword}
+//                         onChange={handleChange}
+//                         required
+//                     />
+//                 </div>
+//                 <div className="form-group">
+//                     <label htmlFor="zipCode">Zip Code</label>
+//                     <input
+//                         type="text"
+//                         className="form-control"
+//                         id="zipCode"
+//                         name="zipCode"
+//                         value={formData.zipCode}
+//                         onChange={handleChange}
+//                         required
+//                     />
+//                 </div>
+//                 <div className="form-group">
+//                     <label htmlFor="state">State</label>
+//                     <select
+//                     className="form-control"
+//                     id="state"
+//                     name="state"
+//                     value={formData.state}
+//                     onChange={handleChange}
+//                     required
+//                 >
+//                     <option value="">Select State</option>
+//                     {/* Map through usStates array to generate options */}
+//                     {usStates.map((state, index) => (
+//                         <option key={index} value={state}>{state}</option>
+//                     ))}
+//                     </select>
+                    
+//                 </div>
+//                 <div className="form-group">
+//             <label htmlFor="country">Country</label>
+//             <select
+//                 className="form-control"
+//                 id="country"
+//                 name="country"
+//                 value={formData.country}
+//                 onChange={handleChange}
+//                 required
+//             >
+//                 <option value="">Select Country</option>
+//                 {/* Dynamically generate country options */}
+//                 {countries.map((country, index) => (
+//                     <option key={index} value={country}>
+//                         {country}
+//                     </option>
+//                 ))}
+//             </select>
+//         </div>
+//                 <div className="form-group">
+//                     <label htmlFor="favoriteStock">Favorite Stock</label>
+//                     <input
+//                         type="text"
+//                         className="form-control"
+//                         id="favoriteStock"
+//                         name="favoriteStock"
+//                         value={formData.favoriteStock}
+//                         onChange={handleChange}
+//                         required
+//                     >
+//                     </input>
+//                 </div>
+//                 <button type="submit" className="btn btn-primary">Sign Up</button>
+//             </form>
+//         </div>
+//     );
+// };
+
+
+// export default SignupForm;
